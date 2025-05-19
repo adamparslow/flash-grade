4caa121bfe6c22acd6b4a579eb53f83de3a061ac
Adam Parslow <adam.parslow@hotmail.com>
2025-05-19 14:34:34 +1000

    Make this a single page app for iphone

--- backend/main.go
+++ backend/main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"backend/handlers"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	log.Println("Starting server setup")
 	port := os.Getenv("PORT")
-	log.Println("Port", port)
 	if port == "" {
 		port = "10000"
 	}
