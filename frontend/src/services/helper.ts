b2df096d616e925b8ad2f65db495e55bf55ea705
Adam Parslow <adam.parslow@hotmail.com>
2025-05-19 14:43:04 +1000

    Revert "Add log for env"
    This reverts commit 7d0310c436cecb464f8e25f99456bfd212682376.

--- frontend/src/services/helper.ts
+++ frontend/src/services/helper.ts
@@ -1,8 +1,6 @@
 const BACKEND_URL = import.meta.env.VITE_API_URL || "http://localhost:10000";
 
 export async function httpFetch(path: string, method: string, body?: any) {
-    console.log(import.meta.env);
-
     const response = await fetch(`${BACKEND_URL}/api/${path}`, {
         method,
         body: body ? JSON.stringify(body) : undefined,
