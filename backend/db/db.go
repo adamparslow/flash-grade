8ea627f91ecb57f71a1e37af782c9771f9f32423
Adam Parslow <adam.parslow@hotmail.com>
2025-10-10 22:47:26 +1100

    Remove dead code

--- backend/db/db.go
+++ backend/db/db.go
@@ -20,27 +20,9 @@ func init() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found — using system environment variables")
 	}
-	// initDB()
 	initSqliteDB()
 
-	// migrateToSqlite()
 }
-
-// func initDB() {
-// 	connStr := os.Getenv("DATABASE_URL") // safer than hardcoding
-// 	var err error
-// 	db, err = sql.Open("postgres", connStr)
-// 	if err != nil {
-// 		log.Fatal("Error opening database:", err)
-// 	}
-
-// 	err = db.Ping()
-// 	if err != nil {
-// 		log.Fatal("Cannot connect to the database:", err)
-// 	}
-
-// 	fmt.Println("Connected to the database.")
-// }
 
 func initSqliteDB() {
 	dbPath := os.Getenv("SQLITE_DB_PATH")
@@ -52,7 +34,7 @@ func initSqliteDB() {
 	}
 
 	// SQLite doesn't need Ping to verify remote connectivity, but it's still safe to check
-	// err = db.Ping()
+	sqliteDb.Ping()
 	if err != nil {
 		log.Fatal("Cannot connect to the database:", err)
 	}
@@ -60,97 +42,8 @@ func initSqliteDB() {
 	fmt.Println("Connected to SQLite database:", dbPath)
 }
 
-// func migrateToSqlite() {
-// 	// Migrate translations
-// 	_, err := sqliteDb.Exec("CREATE TABLE IF NOT EXISTS translations (id SERIAL PRIMARY KEY, tagalog TEXT, english TEXT)")
-// 	if err != nil {
-// 		log.Fatal("Can't create translations table")
-// 	}
-
-// 	translations, err := GetTranslationsFromDB()
-// 	if err != nil {
-// 		log.Fatal("Error getting translations:", err)
-// 	}
-
-// 	for _, translation := range translations {
-// 		_, err := sqliteDb.Exec(
-// 			`INSERT INTO translations (id, tagalog, english)
-// 				VALUES (?, ?, ?)
-// 				ON CONFLICT(id) DO UPDATE SET
-// 				tagalog = excluded.tagalog,
-// 				english = excluded.english;`,
-// 			translation.ID,
-// 			translation.Tagalog,
-// 			translation.English,
-// 		)
-// 		if err != nil {
-// 			log.Fatal("Error inserting translation", err)
-// 		}
-// 	}
-
-// 	fmt.Println("Finished migrating translations")
-
-// 	// Migrate answers
-// 	_, err2 := sqliteDb.Exec("CREATE TABLE IF NOT EXISTS answers (id SERIAL PRIMARY KEY, translation_id VARCHAR(255), correct INT, wrong INT, date TIMESTAMP)")
-// 	if err2 != nil {
-// 		log.Fatal("Error creating answers table", err2)
-// 	}
-
-// 	answers, err := GetAnswersFromDB()
-// 	if err != nil {
-// 		log.Fatal("Error getting answers", err2)
-// 	}
-
-// 	for _, answer := range answers {
-// 		_, err := sqliteDb.Exec(
-// 			`INSERT INTO answers (id, translation_id, correct, wrong, date)
-// 				VALUES (?, ?, ?, ?, ?)
-// 				ON CONFLICT(id) DO UPDATE SET
-// 				translation_id = excluded.translation_id,
-// 				correct = excluded.correct,
-// 				wrong = excluded.wrong,
-// 				date = excluded.date;`,
-// 			answer.ID,
-// 			answer.TranslationId,
-// 			answer.Correct,
-// 			answer.Wrong,
-// 			answer.Date,
-// 		)
-// 		if err != nil {
-// 			log.Fatal("Error inserting translation", err)
-// 		}
-// 	}
-// 	fmt.Println("Finished migrating answers")
-// }
-
 func GetDB() *sql.DB {
 	return sqliteDb
-}
-
-func GetAnswersFromDB() ([]entities.Answer, error) {
-	rows, err := db.Query("SELECT id, translation_id, correct, wrong, date FROM answers")
-
-	if err != nil {
-		return nil, err
-	}
-
-	var answers []entities.Answer
-	defer rows.Close()
-
-	for rows.Next() {
-		var answer entities.Answer
-		err := rows.Scan(&answer.ID, &answer.TranslationId, &answer.Correct, &answer.Wrong, &answer.Date)
-		if err != nil {
-			return nil, err
-		}
-		answers = append(answers, answer)
-	}
-
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return answers, nil
 }
 
 func GetTranslationsFromDB() ([]entities.Translation, error) {
